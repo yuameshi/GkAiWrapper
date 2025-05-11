cb0742092da7ae251d74d02d8c520fefd707a210
糸 <64469437+yuameshi@users.noreply.github.com>
2025-05-11 23:23:36 +0800

    feat: add component `UserPrompt`

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import {StyleSheet, View} from 'react-native';
 import {Session} from './components/Response';
+import {UserPrompt} from './components/UserPrompt';
 
 function App() {
 	return (
 		<View style={[styles.root]}>
+			<UserPrompt />
+			<Session />
+			<UserPrompt />
 			<Session />
 		</View>
 	);
