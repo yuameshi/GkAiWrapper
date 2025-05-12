2a9c76a00ab43464a2c664e9416f7bab7decc67e
糸 <64469437+yuameshi@users.noreply.github.com>
2025-05-12 19:19:02 +0800

    feat: update `CheckPasswordApi` to accept ticket parameter for authorization

--- src/api/login/check-password.ts
+++ src/api/login/check-password.ts
@@ -1,12 +1,18 @@
 import {CheckPasswordApiResponse} from '../../types/check-password';
 
-export const CheckPasswordApi = async () => {
+export const CheckPasswordApi = async (ticket: string) => {
 	const response = await fetch(
 		'https://cas.gdust.edu.cn/cas-api/cas/checkPassword',
+		{
+			headers: {
+				Authorization: `Bearer ${ticket}`,
+			},
+		},
 	);
 
 	const data: CheckPasswordApiResponse = await response.json();
 
+	console.log('CheckPasswordApi response:', data);
 	if (data.code !== 0) {
 		throw new Error(data.msg);
 	} else {
