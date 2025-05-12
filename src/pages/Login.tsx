855c0c4c3615a1d24754c4d0083c9e48ab0e427a
糸 <64469437+yuameshi@users.noreply.github.com>
2025-05-12 19:09:31 +0800

    feat: implement login page & logic of getting qrcode

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,108 @@
+import {useEffect, useState} from 'react';
+import {
+	Alert,
+	Button,
+	Image,
+	Linking,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
+import EventSource from 'react-native-sse';
+import qrcode from 'qrcode-generator';
+
 export const Login = () => {
-	return <></>;
+	const [client, setClient] = useState<string | null>(null);
+	const [qrCode, setQrCode] = useState<string | null>(null);
+	const [ticket, setTicket] = useState<string | null>(null);
+
+	useEffect(() => {
+		const es = new EventSource<
+			'HELLO' | 'LOGIN_SUCCESS_TICKET' | 'SUCCESS'
+		>('https://cas.gdust.edu.cn/cas-api/sse/subscribe');
+
+		es.addEventListener('HELLO', event => {
+			console.log('Client ID Got:', event.data?.slice(0, 10) + '...');
+			setClient(event.data);
+		});
+
+		es.addEventListener('LOGIN_SUCCESS_TICKET', event => {
+			console.log('Ticket Got:', event.data?.slice(0, 10) + '...');
+			setTicket(event.data);
+		});
+
+		es.addEventListener('error', event => {
+			if (event.type === 'error') {
+				console.error('Connection error:', event.message);
+			} else if (event.type === 'exception') {
+				console.error('Error:', event.message, event.error);
+			}
+			Alert.alert(
+				'出现错误',
+				'无法获取配置信息，请检查网络连接或稍后再试。',
+			);
+		});
+
+		return () => {
+			if (es) {
+				es.removeAllEventListeners();
+			}
+		};
+	}, []);
+
+	useEffect(() => {
+		if (!client) {
+			return;
+		}
+		const qr = qrcode(0, 'H');
+		qr.addData('https://cas.gdust.edu.cn/cas/mobieAuth?clientId=' + client);
+		qr.make();
+		const dataURL = qr.createDataURL(4, 8);
+		setQrCode(dataURL);
+	}, [client]);
+
+	const handleOpenDingTalkMobile = () => {
+		if (!client) {
+			return;
+		}
+		Linking.openURL(
+			`dingtalk://dingtalkclient/page/link?url=${encodeURIComponent(
+				'https://cas.gdust.edu.cn/cas/mobieAuth?clientId=' + client,
+			)}`,
+		);
+	};
+
+	return (
+		<View style={styles.root}>
+			{qrCode ? (
+				<>
+					<Image
+						source={{uri: qrCode || '', height: 256, width: 256}}
+					/>
+					<Text style={styles.text}>请使用钉钉客户端扫码</Text>
+					<Button
+						title="打开钉钉客户端 ( Beta )"
+						onPress={handleOpenDingTalkMobile}
+						disabled={!client}
+					/>
+				</>
+			) : (
+				<Text style={styles.text}>请稍后……</Text>
+			)}
+		</View>
+	);
 };
+
+const styles = StyleSheet.create({
+	root: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 16,
+		backgroundColor: '#242424',
+	},
+	text: {
+		fontSize: 16,
+		color: '#fff',
+	},
+});
