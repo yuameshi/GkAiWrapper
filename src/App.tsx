44fc09e6e8cffeeafaf8e5d989a3b98d6aa94f3e
糸 <64469437+yuameshi@users.noreply.github.com>
2025-05-12 17:07:12 +0800

    feat: implement token retrieval and add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import {Alert} from 'react-native';
 import {useEffect, useState} from 'react';
 import {createSession} from './api/create-session';
 import {Main} from './pages/Main';
+import {getToken,setToken as setStoredToken} from './store/token';
+import {Login} from './pages/Login';
 
 function App() {
 	const [id, setId] = useState<number | null>(null);
 	const [sessionId, setSessionId] = useState<string | null>(null);
+	const [token, setToken] = useState<string | null>(null);
 
 	useEffect(() => {
+		getToken().then(storedToken => {
+			if (storedToken) {
+				setToken(storedToken);
+			}
+		});
+	}, []);
+
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
 		createSession()
 			.then(res => {
 				if (res.code === 1) {
@@ -18,6 +32,7 @@ function App() {
 					);
 					setId(res.data.id);
 					setSessionId(res.data.sessionId);
+					setStoredToken(token);
 				} else {
 					console.error('Failed to create session:', res.msg);
 					Alert.alert('创建会话失败', res.msg);
@@ -27,9 +42,9 @@ function App() {
 				console.error('Error creating session:', err);
 				Alert.alert('创建会话失败', '网络错误，请稍后再试');
 			});
-	}, []);
+	}, [token]);
 
-	return id && sessionId ? <Main id={id} sessionId={sessionId} /> : <></>;
+	return id && sessionId ? <Main id={id} sessionId={sessionId} /> : <Login />;
 }
 
 export default App;
